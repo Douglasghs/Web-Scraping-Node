7ad614c3e8ff2193bfaeb07277c5d12db2797f81
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-09-13 11:32:19 -0300

    Adicionando estrutura de dados e tentando resolver questão do click

---
+++ modules/bd.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+const criacaoBD = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "147258"
+});
+
+// Fazendo conexão com a base de dados, passando credenciais citadas logo acima;
+criacaoBD.connect((erro) =>{
+    if(erro){
+        console.log("ERRO: connection database : " + erro);
+    }
+    else{
+        console.log("connection do banco de dadois com sucesso");
+    }
+});
+
+
+// Fazendo a Query de criação da base de dados do programa;
+criacaoBD.query("create database if not exists fuso_horarios", (erro) => {
+    if(erro){
+        console.log("ERRO: Create database : " + erro);
+    }
+    else{
+        console.log("Criação do banco de dados feita com sucesso");
+    }
+});
+
+
+module.exports = criacaoBD;
--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const server = require("http").createServer(app);
 const puppeteer = require('puppeteer');
 
 app.get("/", async (req, res) =>{
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null,
+        args: ['--start-maximixed']
+    });
     const page = await browser.newPage();
-    await page.goto('https://pt-br.facebook.com/');
+    await page.goto('https://www.google.com.br/', {waitUntil: 'networkidle0'});
   
-    await page.type(id="#email", "lalallalala@facebook.com");
-    //await page.click('input[name="btnK"]')
-
+    await page.type("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input", "dolar");
+    await page.click("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b");
+    
     await page.screenshot({ path: 'example.png' });
     await browser.close();
 })
