14636f6978165df3f692b1e953484c73817ed82b
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-09-14 10:19:24 -0300

    criando tabela no SGBD

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 8081;
 const server = require("http").createServer(app);
 const puppeteer = require('puppeteer');
 
+const Modules_BD = require('./modules/bd');
+
 app.get("/", async (req, res) =>{
     const browser = await puppeteer.launch({
         headless: true,
@@ -14,8 +16,27 @@ app.get("/", async (req, res) =>{
     await page.goto('https://www.google.com.br/', {waitUntil: 'networkidle0'});
   
     await page.type("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input", "dolar");
-    await page.click("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b");
-    
+    //const form = await page.$('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b');
+    //await form.$eval(form => form.click());
+
+    const cssSelector = await page.evaluate(() => document.cssSelector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b'))
+    const isElement = async (page, cssSelector) =>{
+        let visible = true;
+        await page
+        .waitForSelector(cssSelector, {visible: true, timeout: 2000})
+        .catch(() =>{
+            visible = false;
+        });
+        return visible;
+    }
+
+    let loadMoreVisible = await isElementVisible(page, cssSelector);
+    while(loadMoreVisible){
+        await page.click(cssSelector);
+        loadMoreVisible = await isElementVisible(page, cssSelector);
+    }
+
+
     await page.screenshot({ path: 'example.png' });
     await browser.close();
 })
