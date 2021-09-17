cb66c84750ed34b8d4aec6f4d3d911a1730194ab
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-09-17 14:44:09 -0300

    Corrigindo bot para pegar valores

--- Controller/Bot.js
+++ Controller/Bot.js
@@ -11,8 +11,14 @@ exports.BotStart = async (req, res) =>{
     await page.goto('https://www.google.com.br/', {waitUntil: 'networkidle0'});
   
     await page.type("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input", `${req.params.money}`);
+    await page.waitForTimeout(4000);
     const form = await page.$('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b');
+    await page.waitForTimeout(4000);
     await form.evaluate(form => form.click());
+    await page.waitForTimeout(4000);
+
+    let nomeMoeda = await page.
+    console.log(nomeMoeda);
 
     await page.screenshot({ path: 'example.png' });
     await browser.close();
