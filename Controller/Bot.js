48eea957e762622233d71cb5822a1e8c0f89c572
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-09-21 09:18:50 -0300

    Conseguindo pegar valor da página web desejada

--- Controller/Bot.js
+++ Controller/Bot.js
@@ -1,27 +1,31 @@
 const puppeteer = require('puppeteer');
 //const Modules_BD = require('./modules/bd');
 
-exports.BotStart = async (req, res) =>{
-    const browser = await puppeteer.launch({
-        headless: true,
-        defaultViewport: null,
-        args: ['--start-maximixed']
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com.br/', {waitUntil: 'networkidle0'});
-  
-    await page.type("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input", `${req.params.money}`);
-    await page.waitForTimeout(4000);
-    const form = await page.$('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b');
-    await page.waitForTimeout(4000);
-    await form.evaluate(form => form.click());
-    await page.waitForTimeout(4000);
+exports.BotStart = async (req, res) => {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            defaultViewport: null,
+            args: ['--start-maximixed']
+        });
+        const page = await browser.newPage();
+        await page.goto('https://www.google.com.br/', { waitUntil: 'networkidle0' });
 
-    let nomeMoeda = await page.
-    console.log(nomeMoeda);
+        await page.type("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input", `${req.params.money}`);
+        await page.waitForTimeout(4000);
+        const form = await page.$('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b');
+        await page.waitForTimeout(4000);
+        await form.evaluate(form => form.click());
+        await page.waitForTimeout(4000);
 
-    await page.screenshot({ path: 'example.png' });
-    await browser.close();
-    
-    res.status(204).json("Executado com sucesso");
+        const valorA = await page.$eval("#knowledge-currency__updatable-data-column > div.b1hJbf > div.dDoNo.ikb4Bb.gsrt.gzfeS > span.DFlfde.SwHCTb", el => el.textContent);
+        console.log(valorA);
+
+        await page.screenshot({ path: 'example.png' });
+        await browser.close();
+
+        res.status(204).json("Executado com sucesso");
+    } catch (error) {
+        console.log(error);
+    }
 }
